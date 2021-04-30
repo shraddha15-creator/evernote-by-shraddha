1f8e25a06e82d199b6c0d4b25c3303fb0a8678cc
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2021-04-30 10:04:45 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const firebase = require ('firebase');
+// require ('firebase/firestore');
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBrA-F4mHsxt-zq3PWfHup3L0CaWow0-qk",
+  authDomain: "evernote-app-4e166.firebaseapp.com",
+  projectId: "evernote-app-4e166",
+  storageBucket: "evernote-app-4e166.appspot.com",
+  messagingSenderId: "896255595579",
+  appId: "1:896255595579:web:39d56b46838cfba770f81e",
+  measurementId: "G-J5B5E8G1ET"
+});
+
+
+
+ReactDOM.render( <App />, document.getElementById('evernote-container'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
